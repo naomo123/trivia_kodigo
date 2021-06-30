099638304a041f20884de3631e72501f5631b326
naomo123 <55609446+naomo123@users.noreply.github.com>
2021-06-29 22:47:51 -0600

    first commit

---
+++ proceso.js
@@ -0,0 +1,94 @@
+let preguntas = [
+    {
+    numb: 1,
+    question: "¿Qué es FTP?",
+    answer: "Protocolo de transferencia de archivos",
+    options: [
+      "Todas las anteriores",
+      "Protocolo de transferencia de archivos",
+      "Protocolo de envío de correo",
+      "Protocolo para conectar ordenadores a internet"
+    ]
+  },
+    {
+    numb: 2,
+    question: "¿Cuál sería el resultado de 3+2+'7'?",
+    answer: "57",
+    options: [
+      "5+'7'",
+      "12",
+      "57",
+      "No se puede realizar"
+    ]
+  },
+    {
+    numb: 3,
+    question: "¿Cuáles de estas son marcas para la inserción del código JavaScript en las páginas HTML?",
+    answer: "< script > y < /script >",
+    options: [
+      "< javascript _code > y < /javascript_code >",
+      "< script > y < /script >",
+      "< ?script > y < script? >",
+      "(script) y (/script)"
+    ]
+  },
+    {
+    numb: 4,
+    question: "¿De las siguientes palabras claves instrucciones una no se utiliza para construir bucles, señala cual es.",
+    answer: "JavaScript diferencia entre mayúsculas y minúsculas",
+    options: [
+      "While",
+      "if",
+      "For",
+      "Do"
+    ]
+  },
+    {
+    numb: 5,
+    question: "¿Cuál es la instrucción usada para devolver un valor en una función de JavaScript?",
+    answer: "Return",
+    options: [
+      "Value",
+      "Send",
+      "Pull",
+      "Return"
+    ]
+  },
+
+  {
+  numb: 6,
+  question: "¿Cuál de estas instrucciones está correctamente escrita en Javascript?",
+  answer: "if (a==0) alert (a);",
+  options: [
+    "if (a==0) alert (a);",
+    "if (a=0) print a;",
+    "if (a==0) { print [a] }",
+    "if (a==0): print a;"
+  ]
+},
+
+{
+numb: 7,
+question: "Para definir el estilo de los elementos que se muestran en la página, utilizamos:",
+answer: "CSS",
+options: [
+  "HTML",
+  "JAVASCRIPT",
+  "PHP",
+  " CSS"
+]
+},
+{
+numb: 8,
+question: "Qué norma rige el Lenguaje de Programación JavaScript?",
+answer: "ECMAScript",
+options: [
+  "Ansi",
+  "ECMAScript",
+  "ISO 9660",
+  "IEC"
+]
+},
+
+
+];
